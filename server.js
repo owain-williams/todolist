4d522e067cfaa62c80f29f071a152365c957c989
owain-williams <owain@andersdigital.com>
2022-07-12 12:02:19 +0100

    Add todo to console

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const app = express()
 const PORT = 3000
 
 app.set("view engine", "ejs")
-app.use(bodyParser({urlencoded: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    res.render('index', {name: 'Owain'})
+    let today = new Date()
+    const dateOptions = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    let day = today.toLocaleString('en-us', dateOptions)
+    res.render('index', {day: day})
+})
+
+app.post('/', (req, res) => {
+    newTask = req.body.newTask
+    console.log(newTask)
 })
 
 app.listen(PORT, console.log(`The app is listening on port ${ PORT }`))
